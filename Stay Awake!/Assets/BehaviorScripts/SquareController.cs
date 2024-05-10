e17377b5d72c0a607601728bb8ce941a112615ed
Shawn Guo <sguo37@jhu.edu>
2024-05-09 21:12:01 -0400

    5/9/24.2
    -ball prefabs and hooked to each square

--- Stay Awake!/Assets/BehaviorScripts/SquareController.cs
+++ Stay Awake!/Assets/BehaviorScripts/SquareController.cs
@@ -8,6 +8,11 @@ public class SquareController : MonoBehaviour
     [Header("DataCube Refs")]
     public Square square;
     public PlayerState playerState;
+
+    [Header("Objects")]
+    [SerializeField] private GameObject anyBall;
+    [SerializeField] private GameObject energyBall;
+    [SerializeField] private GameObject enemyBall;
     public void UpdateHasPlayer() {
         if (playerState.onCol == square.squareCol && playerState.onRow == square.squareRow) {
             square.hasPlayer = true;
